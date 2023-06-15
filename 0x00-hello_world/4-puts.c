7edc59988ae8bbfe8b1a6b7521cfa5cdcae7872f
FaithRono <faithrono132@gmail.com>
2023-06-15 05:37:49 -0700

    funtion of puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- *main - main entry poit
- *main show the entry point
- *return : alwasys 0 (success)
- *this program include the function of puts
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
