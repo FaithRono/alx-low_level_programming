2e97ba196dce8b3b39822a5c1b6473f1da74f63e
FaithRono <faithrono132@gmail.com>
2023-07-03 07:22:02 -0700

    searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,10 +2,10 @@
 
 /**
  * _strpbrk - Search a string for any of a set of bytes.
-
+ *
  * @s: string
  * @accept: string to match
-
+ *
  * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
  * or NULL if no such byte is found.
  */
