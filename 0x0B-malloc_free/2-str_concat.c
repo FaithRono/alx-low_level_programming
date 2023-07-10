9b3f3051d23c6bed10d763137ff4f9bfa174410e
FaithRono <faithrono132@gmail.com>
2023-07-10 08:57:28 -0700

    function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,44 +3,38 @@
 #include <stdio.h>
 
 /**
-  * str_concat - function that concatenates two strings.
-  * @s1: the first string to be concatenated
-  * @s2: the second string to be concatenated
-  *
-  * Return: the two strings concatenated
-  */
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ *
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int a = 0, b = 0, c = 0, d = 0;
-	char *f;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[a])
-		a++;
-	while (s2[b])
-		b++;
-	d = a + b;
-	f = malloc((sizeof(char) * 1) + 1);
 
-	if (f == NULL)
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
 		return (NULL);
-	b = 0;
 
-	while (c < d)
-	{
-		if (c <= a)
-		f[c] = s1[c];
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
 
-		if (c >= a)
-		{
-			f[c] = s1[b];
-			b++;
-		}
-		c++;
-	}
-	f[c] = '\0';
-	return (f);
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }
