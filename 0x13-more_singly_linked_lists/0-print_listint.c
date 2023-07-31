49e59071f3f9d54a1001ceafa32415cc4cb37507
Faith Cherono <faithrono132@gmail.com>
2023-07-31 09:25:48 -0700

    function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
   * print_listint - function that prints all the
   *                 elements of a listint_t list.
   * @h: pointer to the head of the list
-  *  Return: number of nodes
+  * Return: number of nodes
   */
 size_t print_listint(const listint_t *h)
 {
