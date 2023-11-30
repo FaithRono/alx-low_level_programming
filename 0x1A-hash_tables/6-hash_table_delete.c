c34139d49369eb3a473bf6b908692b9a7733e296
FaithRono <faithrono132@gmail.com>
2023-11-30 07:28:55 -0800

    hash table delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,26 +7,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int idx;
-    hash_node_t *node, *temp;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-    if (ht == NULL)
-        return;
-
-    for (idx = 0; idx < ht->size; idx++)
-    {
-        node = ht->array[idx];
-        while (node != NULL)
-        {
-            temp = node->next;
-            free(node->key);
-            free(node->value);
-            free(node);
-            node = temp;
-        }
-    }
-
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
 }
-
